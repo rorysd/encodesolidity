a2b296a520121e59f92e08cbfb5a0366adfbcc66
Rory <rory.spies@gmail.com>
2021-11-15 19:58:58 +0000

    homework 4

--- VolcanoCoin.sol
+++ VolcanoCoin.sol
@@ -2,13 +2,23 @@
 
 contract VolcanoCoin {
     
-    uint totalSupply = 10000;
+    mapping(address => uint256) private _balances;
+    mapping(address => Payment[]) private _payments;
+    
+    uint256 private _totalSupply = 10000;
     address owner;
     
+    struct Payment {
+        address paymentRecipient;
+        uint paymentAmount;
+    }
+    
     event SupplyIncreased(uint _newSupply);
+    event TransactionSuccess(uint transactionAmount, address transactionAddress);
     
     constructor(){
         owner = msg.sender;
+        _balances[owner] = _totalSupply;
     }
     
     modifier onlyOwner {
@@ -17,12 +27,38 @@ contract VolcanoCoin {
         }
     }
     
+    function getPayments (address account) public view virtual returns (Payment[] memory) {
+        return _payments[account];
+    }
+    
+    function balanceOf(address account) public view virtual returns (uint256) {
+        return _balances[account];
+    }
+    
     function getSupply() public view returns (uint) {
-        return totalSupply;
+        return _totalSupply;
     }
     
     function increaseSupply() public onlyOwner {
-        totalSupply += 1000;
-        emit SupplyIncreased(totalSupply);
+        _totalSupply += 1000;
+        _balances[owner] = _totalSupply;
+        emit SupplyIncreased(_totalSupply);
+    }
+    
+    function transfer(address recipient, uint256 amount) public virtual returns (bool) {
+        
+        if (amount <= _balances[msg.sender]){
+            _balances[msg.sender] = _balances[msg.sender] -= amount;
+            _balances[recipient] = _balances[recipient] += amount;
+            _payments[msg.sender].push(
+                Payment({
+                paymentRecipient: recipient,
+                paymentAmount: amount
+            }));
+            emit TransactionSuccess(amount, recipient);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
